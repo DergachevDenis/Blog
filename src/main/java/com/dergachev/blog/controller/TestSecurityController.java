d751e0575ddaae8ff8feae36fa3167e8df17004c
DergachevDenis <dergache.di@gmail.com>
2021-06-21 21:24:03 +0300

    fix logger

--- src/main/java/com/dergachev/blog/controller/TestSecurityController.java
+++ src/main/java/com/dergachev/blog/controller/TestSecurityController.java
@@ -1,11 +1,14 @@
 package com.dergachev.blog.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestSecurityController {
 
